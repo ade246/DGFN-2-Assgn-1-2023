ecd809aac39b69d0136bce5b4fe42cdeffe1a171
Khari Wallace <khari.wallace@dcmail.ca>
2023-10-14 01:05:16 -0400

    AV CALC

---
+++ test_A_V_calc.py
@@ -0,0 +1,14 @@
+'''
+TPRG 2131 Fall 2023 Assignment 1
+Sept, 2023
+Phil J <philip.jarvis@durhamcollege>
+
+
+This file is to be the test_A_V_calc.py file to be used with your A_V_calc.py and pytest.
+This must test each of your Area/Volume functions, with a least 3 examples for each of the
+5 functions your chose to use.
+
+As this stands it will NOT work with your A_V_calc.py file.
+'''
+
+from assertions import add2
