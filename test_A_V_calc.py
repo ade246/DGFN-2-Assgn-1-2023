753f89a8d3c0d817592391e0c73f99d2d4ccae3a
Khari Wallace <khari.wallace@dcmail.ca>
2023-10-13 22:42:43 -0400

    first commit - correct files going to repository now

--- test_A_V_calc.py
+++ test_A_V_calc.py
@@ -1,63 +1,14 @@
 '''
-# test_A_V_calc.py
-
 TPRG 2131 Fall 2023 Assignment 1
-October,11 2023
-KHARI WALLACE, 100807131,
-Khari Wallace <khari.wallace@dcmail.ca>
+Sept, 2023
+Phil J <philip.jarvis@durhamcollege>
+
+
+This file is to be the test_A_V_calc.py file to be used with your A_V_calc.py and pytest.
+This must test each of your Area/Volume functions, with a least 3 examples for each of the
+5 functions your chose to use.
+
+As this stands it will NOT work with your A_V_calc.py file.
 '''
 
 from assertions import add2
-import A_V_calc
-import pytest
-
-
-
-def test_calculate_volume_1():
-    # Test calculation 1 (Sphere volume)
-    result = A_V_calc.calculate_volume_1([3.0])  # Replace with the appropriate radius
-    assert result == 200  # expected result
-    assert result == 113.09733552923254  # Replace with expected result
-    assert result == 116.29333552923254  # Replace with the expected result
-
-
-
-def test_calculate_volume_2():
-    # Test calculation 2 (Cube volume)
-    result = A_V_calc.calculate_volume_2([2.0])  # Replace with the appropriate side length
-    assert result == 10.0  # expected result
-    assert result == 8.0  # Replace with the expected result
-    assert result == 14.0  # Replace with the expected result
-
-
-
-def test_calculate_area_1():
-    # Test calculation 3 (Rectangle area)
-    result = A_V_calc.calculate_area_1([4.0, 5.0])  # Replace with the appropriate length and width
-    assert result == 10.0  # expected result
-    assert result == 20.0  # Replace with the expected result
-    assert result == 40.0  # Replace with the expected result
-
-
-
-def test_calculate_area_2():
-    # Test calculation 4 (Triangle area)
-    result = A_V_calc.calculate_area_2([6.0, 8.0])  # Replace with the appropriate base and height
-    assert result == 10.0  # Replace with the expected result
-    assert result == 24.0  # expected result
-    assert result == 14.0  # Replace with the expected result
-
-
-
-def test_calculate_area_3():
-    # Test calculation 5 (Trapezoid area)
-    result = A_V_calc.calculate_area_3([5.0, 7.0, 10.0])  # Replace with the appropriate base1, base2, and height
-    assert result == 75.0  # Replace with the expected result
-    assert result == 55.0  # Replace with the expected result
-    assert result == 60.0  # expected result
-
-
-
-
-if __name__ == '__main__':
-    pytest.main()
